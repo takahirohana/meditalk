1513f6149cd99b4f10546a1ceb33043979ac3d75
takahirohana <w.hanasta.w@gmail.com>
2020-10-14 17:09:14 +0900

    メッセージの表示

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   # get 'symptoms/index'
   root to:'rooms#index'  
   resources :users, only: [:new, :edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create, :delete] do
+    resources :messages, only: [:index, :create]
+  end
 end
 
 #   root to: "symptoms#index"
