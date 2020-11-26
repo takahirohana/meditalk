d36954cf0fae4fe0650e9e1fe69ffcc69e4f08cc
takahirohana <w.hanasta.w@gmail.com>
2020-11-26 23:37:49 +0900

    環境変数をRailsアプリケーション側で読み込むように記述を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'taka' && password == '1111'
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
   def configure_permitted_parameters
