31aca81456d2891b9c79c0da7e25aee4058e1e0f
takahirohana <w.hanasta.w@gmail.com>
2020-11-26 23:12:23 +0900

    controllers/application_controller.rbの記述をする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'taka' && password == '1111'
+    end
+  end
   def configure_permitted_parameters
     if resource_class == User
       devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :age])
