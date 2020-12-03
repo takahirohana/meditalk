0105588aa682bde1ee7ec24e4ebcc08616513383
takahirohana <w.hanasta.w@gmail.com>
2020-12-03 15:32:15 +0900

    web socketアクセス権限

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,8 +40,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'ws://52.192.99.87//cable'
+  config.action_cable.allowed_request_origins = [ 'http://52.192.99.87', /http:\/\/52.192.99.87*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
